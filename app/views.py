89e52f93a918ec3cd179e12e6095a227d4fdec38
Anthony Lackey <alackey96@gmail.com>
2015-09-15 08:07:38 -0700

    Able to get saved posts

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import request
-import praw, webbrowser
+from flask import jsonify
+from pprint import pprint
+import praw, webbrowser, requests
 
 @app.route('/')
 @app.route('/index')
@@ -9,24 +11,39 @@ def index():
     return rule.rule
 
 r = praw.Reddit('OAuth testing example by u/Tech_Runmner ver 0.1')
+code = ''
+access_token = ''
 @app.route('/login')
 def login():
     r.set_oauth_app_info(client_id='R0RrAiBvmgk-3Q',
-                         client_secret='YPVjlyJoon9CsONiCU4TnJK8-Z8',
+                         client_secret='',
                          redirect_uri='http://127.0.0.1:5000/'
                                       'authorize_callback')
-    url = r.get_authorize_url('uniqueKey', 'identity', True)
+    url = r.get_authorize_url('uniqueKey', 'identity read save history', True)
     webbrowser.open(url)
     return "login"
 
+
 @app.route('/authorize_callback')
 def authorized():
+    #Changes the user from me to authenticated user
     access_information = r.get_access_information(request.args.get('code', ''))
-    authenticated_user = r.get_me()
+    code = request.args.get('code', '')
     return "hello"
 
 
 @app.route('/loginsuccess')
 def success():
-    #access_information = r.get_access_information('8aunZCxfv8mcCfD8no4CSlO55u0')
     return "login success"
+
+@app.route('/savedposts')
+def getSaved():
+    reddit_saved_links = []
+    reddit_saves = r.user.get_saved(limit=4)
+    #add the reddit saved posts to list
+    for reddit_save in reddit_saves:
+        reddit_saved_links.append(reddit_save)
+
+    for thing in reddit_saved_links:
+        print(thing)
+    return 'done'
