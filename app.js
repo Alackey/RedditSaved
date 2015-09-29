955e9f6910bec068b7443232527168b8dd33f227
Anthony Lackey <alackey96@gmail.com>
2015-09-28 22:41:23 -0700

    Major overhaul, Flask.py -> node/express.js. Uses cookies and redirects. Very messy but caught up.

---
+++ app.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+var Snoocore = require('snoocore');
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+var uniqId = 0; // unique id for our user
+var accounts = {}; // Snoocore instances. Each instance is a different account
+
+function getInstance(accountId) {
+
+    // Check if we already have an instance with this id. If
+    // so, use this instance
+    if (accounts[accountId]) {
+        return accounts[accountId];
+    }
+
+    // Else, return a new instance
+    return new Snoocore({
+        userAgent: 'test-express-web / snoocore-examples',
+        oauth: {
+            type: 'explicit',
+            duration: 'permanent',
+            key: 'R0RrAiBvmgk-3Q', // not an actual working key, use your own
+            secret: 'YPVjlyJoon9CsONiCU4TnJK8-Z8', // not an actual working secret, use your own
+            redirectUri: 'http://localhost:3000/reddit_redirect',
+            scope: [ 'identity save read history' ]
+        }
+    });
+}
+
+app.get('/', function (req, res) {
+    var accountId = req.cookies ? req.cookies.account_id : void 0;
+
+    // We have an account, redirect to the authenticated route
+    if (accountId) {
+        return res.redirect('/me');
+    }
+
+    var reddit = getInstance();
+    return res.send('<a href="' + reddit.getAuthUrl() + '">Authenticate!</a>');
+});
+
+app.get('/me', function(req, res) {
+
+    var accountId = req.cookies ? req.cookies.account_id : void 0;
+
+    // If the user has not authenticated bump them back to the main route
+    if (!accountId) {
+        return res.redirect('/');
+    }
+
+    // Print out stats about the user, that's it.
+    // return accounts[accountId]('/api/v1/me').get().then(function(result) {
+    //     // //Get saved posts
+    //     // accounts[accountId]('/user/' + result.name + '/saved').get().then(function(posts) {
+    //     //     var redditPosts = [];
+    //     //     console.log(posts.data.children[0].data.id);
+    //     //     for( var i = 0; i < posts.data.children.length; i++){
+    //     //         if (posts.data.children[i].data.body != undefined) {
+    //     //             redditPosts.push({ 'info': [
+    //     //                                 { 'type': 'Comment'},
+    //     //                                 { 'mainText': posts.data.children[i].data.body },
+    //     //                                 { 'permalink': "www.reddit.com/comments/" +
+    //     //                                     posts.data.children[i].data.link_id.split("_")[1] +
+    //     //                                     "/x/" + posts.data.children[i].data.id}
+    //     //                             ]});
+    //     //             //Post is a Comment
+    //     //             //console.log(posts.data.children[i].data.body);
+    //     //         } else {
+    //     //             redditPosts.push({ 'info': [
+    //     //                                 { 'type': 'Submission'},
+    //     //                                 { 'mainText': posts.data.children[i].data.title },
+    //     //                                 { 'permalink': "www.reddit.com" + posts.data.children[i].data.permalink},
+    //     //                                 { 'titleLink': posts.data.children[i].data.url}
+    //     //                             ]});
+    //     //             //Post is a Submission
+    //     //             //console.log(posts.data.children[i].data.title);
+    //     //         }
+    //     //
+    //     //     }
+    //     //     console.log(JSON.stringify(redditPosts, null, 4));
+    //     // });
+    //
+    //     return res.send(JSON.stringify(result, null, 4));
+    // });
+    return res.render('index.html')
+});
+
+app.get('/savedposts', function(req, res) {
+    return accounts[req.cookies.account_id]('/api/v1/me').get().then(function(result) {
+        //Get saved posts
+        accounts[req.cookies.account_id]('/user/' + result.name + '/saved').get().then(function(posts) {
+            var redditPosts = [];
+            console.log(posts.data.children[0].data.id);
+            for( var i = 0; i < posts.data.children.length; i++){
+                if (posts.data.children[i].data.body != undefined) {
+                    redditPosts.push({ 'info': [
+                                        { 'type': 'Comment'},
+                                        { 'mainText': posts.data.children[i].data.body },
+                                        { 'permalink': "http://www.reddit.com/comments/" +
+                                            posts.data.children[i].data.link_id.split("_")[1] +
+                                            "/x/" + posts.data.children[i].data.id}
+                                    ]});
+                    //Post is a Comment
+                    //console.log(posts.data.children[i].data.body);
+                } else {
+                    redditPosts.push({ 'info': [
+                                        { 'type': 'Submission'},
+                                        { 'mainText': posts.data.children[i].data.title },
+                                        { 'permalink': "http://www.reddit.com" + posts.data.children[i].data.permalink},
+                                        { 'titleLink': posts.data.children[i].data.url}
+                                    ]});
+                    //Post is a Submission
+                    //console.log(posts.data.children[i].data.title);
+                }
+
+            }
+            //console.log(JSON.stringify(redditPosts, null, 4));
+            console.log("about to send posts");
+            return res.json(redditPosts);
+        });
+    });
+});
+
+// does not account for hitting "deny" / etc. Assumes that
+// the user has pressed "allow"
+app.get('/reddit_redirect', function(req, res) {
+
+    var accountId = ++uniqId; // an account id for this instance
+    var instance = getInstance(); // an account instance
+
+    // In a real app, you would save the refresh token in
+    // a database / etc for use later so the user does not have
+    // to allow your app every time...
+    return instance.auth(req.query.code).then(function(refreshToken) {
+        // Store the account (Snoocore instance) into the accounts hash
+        accounts[accountId] = instance;
+
+        // Set the account_id cookie in the users browser so that
+        // later calls we can refer to the stored instance in the
+        // account hash
+
+
+        console.log(accounts);
+        res.cookie('account_id', String(accountId), { maxAge: 900000, httpOnly: true });
+
+        // redirect to the authenticated route
+        return res.redirect('/me');
+    });
+
+});
+
+var server = app.listen(3000, function () {
+  console.log('Example app listening at http://localhost:3000');
+});
---
+++ public/javascripts/actions.js
@@ -0,0 +1,53 @@
+//Loads the homepage posts on page load
+function homeOnLoad(){
+    $.get('/savedposts', {
+    }).done(function(posts) {
+        console.log("homeOnLoad started");
+        console.log(posts);
+        console.log(posts[3].info[2].permalink);
+        for(var i = 0; i < posts.length; i++){
+            var div = document.createElement('div');
+            div.id = 'posts';
+            if ( posts[i].info[0].type == 'Submission'){
+                div.innerHTML = '<a href="' + posts[i].info[3].titleLink + '"><h3>'
+                                + posts[i].info[1].mainText + '</h3></a>' +
+                                '<p>' + posts[i].info[0].type + '</p>' +
+                                '<a href="' + posts[i].info[2].permalink + '">Comments</a>' +
+                                '<a href="#">unsave</a>' ;
+            } else {
+                div.innerHTML = '<h4>' + posts[i].info[1].mainText + '</h4>' +
+                                '<p>' + posts[i].info[0].type + '</p>' +
+                                '<a href="' + posts[i].info[2].permalink + '">permalink</a>' +
+                                '<a href="#">unsave</a>';
+            }
+            $("#javascriptposts").hide().append(div).fadeIn(350);
+        }
+        console.log("homeOnLoad finished");
+    });
+}
+window.onload = homeOnLoad;
+
+function change(sourceId, btnId) {
+    $(btnId).hide();
+    $.get('/change', {
+    }).done(function(changed) {
+        $(sourceId).text(changed['text'])
+    }).fail(function() {
+        $(btnid).show();
+    });
+}
+
+function clear(postsId) {
+    $(postsId).empty();
+    $.get('/clear', {
+    }).done(function(changed) {
+        for(var i = 0; i < changed.length; i++){
+            var div = document.createElement('div');
+            div.id = 'posts';
+            div.innerHTML = '<p>' + changed[i].text + '</p>';
+            $("#javascriptposts").append(div).hide().fadeIn(100);
+        }
+    }).fail(function() {
+        $(postsId).show();
+    });
+}
