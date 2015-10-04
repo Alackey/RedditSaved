c73764d3b6ae70985e5b477601d65635b18b42e8
Anthony Lackey <alackey96@gmail.com>
2015-10-04 13:19:00 -0700

    Basic mongodb/mongoose integration added

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var app = express();
+//var appDB = express();
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 var Snoocore = require('snoocore');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/RedditSaved');
+console.log(mongoose.connection.host);
+console.log(mongoose.connection.port);
+console.log(mongoose.connection.collections);
+
+//Check to see if connected
+mongoose.connection.on('connected', function () {
+    console.log('hello');
+});
+
+var FormSchema = new mongoose.Schema({
+    name: String,
+    created: Date
+}, {collection: "form"});
+
+//create model/collection
+var Form = mongoose.model("Form", FormSchema);
+
+var form1 = new Form({name: "Form 1", created: new Date()});
+
+form1.save();
+
+Form.find( function (err, result) {
+    console.log(result);
+});
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
@@ -34,6 +62,7 @@ function getInstance(accountId) {
     });
 }
 
+
 app.get('/', function (req, res) {
     var accountId = req.cookies ? req.cookies.account_id : void 0;
 
@@ -55,40 +84,6 @@ app.get('/me', function(req, res) {
         return res.redirect('/');
     }
 
-    // Print out stats about the user, that's it.
-    // return accounts[accountId]('/api/v1/me').get().then(function(result) {
-    //     // //Get saved posts
-    //     // accounts[accountId]('/user/' + result.name + '/saved').get().then(function(posts) {
-    //     //     var redditPosts = [];
-    //     //     console.log(posts.data.children[0].data.id);
-    //     //     for( var i = 0; i < posts.data.children.length; i++){
-    //     //         if (posts.data.children[i].data.body != undefined) {
-    //     //             redditPosts.push({ 'info': [
-    //     //                                 { 'type': 'Comment'},
-    //     //                                 { 'mainText': posts.data.children[i].data.body },
-    //     //                                 { 'permalink': "www.reddit.com/comments/" +
-    //     //                                     posts.data.children[i].data.link_id.split("_")[1] +
-    //     //                                     "/x/" + posts.data.children[i].data.id}
-    //     //                             ]});
-    //     //             //Post is a Comment
-    //     //             //console.log(posts.data.children[i].data.body);
-    //     //         } else {
-    //     //             redditPosts.push({ 'info': [
-    //     //                                 { 'type': 'Submission'},
-    //     //                                 { 'mainText': posts.data.children[i].data.title },
-    //     //                                 { 'permalink': "www.reddit.com" + posts.data.children[i].data.permalink},
-    //     //                                 { 'titleLink': posts.data.children[i].data.url}
-    //     //                             ]});
-    //     //             //Post is a Submission
-    //     //             //console.log(posts.data.children[i].data.title);
-    //     //         }
-    //     //
-    //     //     }
-    //     //     console.log(JSON.stringify(redditPosts, null, 4));
-    //     // });
-    //
-    //     return res.send(JSON.stringify(result, null, 4));
-    // });
     return res.render('index.html')
 });
 
@@ -127,6 +122,35 @@ app.get('/savedposts', function(req, res) {
         });
     });
 });
+
+// app.get('/test', function(req, res) {
+//     console.log('1');
+//
+//     var chris = new User({
+//       name: 'Anthony',
+//       username: 'alackey',
+//       text: 'This worked!'
+//     });
+//
+//     console.log('2');
+//
+//     chris.save(function(err) {
+//       if (err) throw err;
+//
+//       console.log('User saved successfully!');
+//     });
+//
+//     User.find({ username: 'alackey' }, function(err, user) {
+//       if (err) throw err;
+//
+//       // object of the user
+//       console.log(user);
+//       return 'hello';
+//     });
+//
+//     return 'did it work';
+//
+// });
 
 // does not account for hitting "deny" / etc. Assumes that
 // the user has pressed "allow"
