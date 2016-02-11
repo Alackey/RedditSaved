3cf70485dbb1e86c492aa1af23f9b84a2f59bee7
Anthony Lackey <alackey96@gmail.com>
2016-02-11 11:12:17 -0800

    praw auth working

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^callback/$', views.callback, name='callback'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import praw
+
+user_agent = "Reddit Saved 0.1 by /u/Tech_Runner"
+r = praw.Reddit(user_agent=user_agent)
 
 
 def home(request):
-    return render(request, 'webapp/home.html', {})
+    r.set_oauth_app_info(
+        client_id='R0RrAiBvmgk-3Q',
+        client_secret='YPVjlyJoon9CsONiCU4TnJK8-Z8',
+        redirect_uri='http://127.0.0.1:8000/callback'
+    )
+
+    return render(
+        request,
+        'webapp/home.html',
+        {'authURL': r.get_authorize_url('uniqueKey', 'identity', True)}
+    )
+
+
+def dashboard(request):
+    return render(request, 'webapp/dashboard.html')
+
+
+def callback(request):
+    authorize(request.GET['code'])
+    return redirect('/dashboard/')
+
+
+def authorize(code):
+    access_information = r.get_access_information(code)
+    user = r.get_me()
+    print(user.name, user.link_karma)
