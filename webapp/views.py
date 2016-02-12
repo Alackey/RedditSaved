767f33e99b3f61077f20a91dfa0210f006d8d5d4
Anthony Lackey <alackey96@gmail.com>
2016-02-11 19:59:06 -0800

    Basic home page

--- webapp/views.py
+++ webapp/views.py
@@ -9,7 +9,7 @@ def home(request):
     r.set_oauth_app_info(
         client_id='R0RrAiBvmgk-3Q',
         client_secret='YPVjlyJoon9CsONiCU4TnJK8-Z8',
-        redirect_uri='http://127.0.0.1:8000/callback'
+        redirect_uri='http://127.0.0.1:8000/callback/'
     )
 
     return render(
@@ -26,6 +26,13 @@ def dashboard(request):
 def callback(request):
     authorize(request.GET['code'])
     return redirect('/dashboard/')
+
+
+'''
+
+    Functions
+
+'''
 
 
 def authorize(code):
